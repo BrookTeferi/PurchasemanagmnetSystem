7db8bd11cf709c21fc721cc38b60e6a2f38746f4
brookteferi1q2w@gmail.com <brookteferi1q2w@gmail.com>
2022-08-24 09:59:19 +0300

    last

--- PurchasemanagmnetSystem/Migrations/20220824065508_last.Designer.cs
+++ PurchasemanagmnetSystem/Migrations/20220824065508_last.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PurchasemanagmnetSystem.Models;
 
 namespace PurchasemanagmnetSystem.Migrations
 {
     [DbContext(typeof(ConnectionStringClass))]
-    partial class ConnectionStringClassModelSnapshot : ModelSnapshot
+    [Migration("20220824065508_last")]
+    partial class last
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ PurchasemanagmnetSystem/Migrations/20220824065508_last.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PurchasemanagmnetSystem.Migrations
+{
+    public partial class last : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "item_status",
+                table: "items",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "item_status",
+                table: "items");
+        }
+    }
+}
