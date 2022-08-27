6a0806890c75f3ac2e2d5bcd8e5cd2deb5c821ce
Brook <Brook@DESKTOP-20AUB7L>
2022-08-27 11:10:11 +0300

    deleted data

--- newtesfesh/datalayer/tesfaye.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace newtesfesh.datalayer
-{
-    public class tesfaye
-    {
-        public int id { get; set; }
-        public int first { get; set; }
-        public int id { get; set; }
-    }
-}
